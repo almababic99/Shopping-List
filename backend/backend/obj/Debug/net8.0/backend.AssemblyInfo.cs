8a861218691bcefe54ab28da8f646da819a5550b
Alma Babic <alma.babic@tacta.io>
2025-01-07 14:32:28 +0100

    Project structure

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1997ea6d78050a2be53683bd2254e8b19f4157")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68049fa7e5ec272f47d3c984c339d5a343e5fa21")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
